7184e043749af40d488a5ad405294059e47deb55
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-08-03 01:05:52 -0300

    chore: ajusta configuraçãoes de build para produção

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,34 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true
+      },
     ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 }
